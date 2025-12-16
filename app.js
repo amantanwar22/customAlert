809342f68c3f6242a9747d04b5fef6b30cdfa67f
Aman Tanwar <amantanwar1055@gmail.com>
2025-12-16 17:01:52 +0530

    changed

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
- function showPointsAlert(points) {
-        $("#pointsAlert .msg").text("You earned " + points + " points!");
-        $("#pointsAlert").fadeIn(300);
-    setTimeout(function() {
-            $("#pointsAlert").fadeOut(300);
-    }, 4000);
-    }
+function pointsAlert(message, duration = 4000) {
+    $("#pointsAlert .msg").text(message);
+    $("#pointsAlert").fadeIn(300);
 
-    $("#earnBtn").click(function(){
-        showPointsAlert(20);
-    });
-
-   
-    $("#pointsAlert .closeBtn").click(function(){
+    setTimeout(function () {
         $("#pointsAlert").fadeOut(300);
-    });
+    }, duration);
+}
 
-    
+$("#earnBtn").click(function () {
+    pointsAlert("You earned 20 points!");
+});
+
+$("#pointsAlert .closeBtn").click(function () {
+    $("#pointsAlert").fadeOut(300);
+});
+
+
